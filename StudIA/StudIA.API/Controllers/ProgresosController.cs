c7e15376f00fe759501d1b32e3f1f54dae56abde
WilsonMerlo <julimerlo8@gmail.com>
2026-06-03 13:57:01 -0300

    Fixes front e implementacion controllers y service de apunte

---
+++ StudIA/StudIA.API/Controllers/ProgresosController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using StudIA.Business;
+using StudIA.Data.Entities;
+
+namespace StudIA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgresosController : ControllerBase
+    {
+        private readonly ProgresoService _progresoService;
+
+        public ProgresosController(ProgresoService progresoService)
+        {
+            _progresoService = progresoService;
+        }
+
+        [HttpGet("usuario/{idUsuario}")]
+        public async Task<ActionResult<IEnumerable<Progreso>>> GetProgresosUsuario(int idUsuario)
+        {
+            var progresos = await _progresoService.ObtenerProgresosPorUsuarioAsync(idUsuario);
+            return Ok(progresos);
+        }
+
+        [HttpPost("actualizar")]
+        public async Task<ActionResult<Progreso>> ActualizarProgreso([FromBody] UpdateProgresoRequest request)
+        {
+            var progreso = await _progresoService.ActualizarProgresoAsync(request.IdUsuario, request.IdMateria, request.AvancePorcentual);
+            return Ok(progreso);
+        }
+    }
+
+    public class UpdateProgresoRequest
+    {
+        public int IdUsuario { get; set; }
+        public int IdMateria { get; set; }
+        public float AvancePorcentual { get; set; }
+    }
+}
